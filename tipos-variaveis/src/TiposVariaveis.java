e4a9d25e5889f1fec5e602224253d9bd271bb12d
Bruno <br9uno@gmail.com>
2023-06-08 17:29:00 -0300

    Operadores

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        final String hello = "Hello, World!";
+        int numero = 0;
+        double numeroDecimal = 0.10;
+        float numeroDecimalMenor = 1.23F;
+        long numeroGrande = 131654654654654L;
+        char caractere = 'C';
+        String texto = "Escrever texto";
+        boolean verdadeiroFalso = true;
+
+        System.out.println(hello);
+        System.out.println(numero);
+        System.out.println(numeroDecimal);
+        System.out.println(numeroDecimalMenor);
+        System.out.println(numeroGrande);
+        System.out.println(caractere);
+        System.out.println(hello);
+        System.out.println(texto);
+        System.out.println(verdadeiroFalso);
+    }
+}
